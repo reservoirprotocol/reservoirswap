93c23b0e9d3af7806f1172834c623e5158f5f5e9
stephankmin <stephankmin@gmail.com>
2024-12-02 16:19:41 -0500

    chore: add hh config and deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,11 @@ const config: HardhatUserConfig = {
     ]
   },
   solidity: {
-    compilers: [{ version: "0.8.24", }],
+    compilers: [{
+      version: "0.8.17", settings: {
+        viaIR: true
+      }
+    }],
     overrides: {
       "contracts/CrossChainAccount.sol": {
         version: "0.7.6",
@@ -34,24 +38,189 @@ const config: HardhatUserConfig = {
           optimizer: {
             enabled: true,
             runs: 999999
-          }
-        }
+          },
+        },
       },
       "contracts/v2-core/UniswapV2Pair.sol": {
-        version: "0.5.16"
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
       },
       "contracts/v2-core/UniswapV2ERC20.sol": {
-        version: "0.5.16"
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
       },
       "contracts/v2-core/libraries/UQ112x112.sol": {
-        version: "0.5.16"
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
       },
       "contracts/v2-core/libraries/Math.sol": {
-        version: "0.5.16"
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
       },
       "contracts/v2-core/libraries/SafeMath.sol": {
-        version: "0.5.16"
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
       },
+      "contracts/v2-core/interfaces/IUniswapV2Factory.sol": {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-core/interfaces/IUniswapV2Pair.sol": {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-core/interfaces/IUniswapV2ERC20.sol": {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-core/interfaces/IERC20.sol": {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-core/interfaces/IUniswapV2Callee.sol": {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/UniswapV2Migrator.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/libraries/SafeMath.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/interfaces/IUniswapV2Migrator.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/UniswapV2Router02.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/UniswapV2Router01.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/libraries/UniswapV2LiquidityMathLibrary.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/libraries/UniswapV2Library.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/interfaces/IUniswapV2Router01.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "contracts/v2-periphery/interfaces/IUniswapV2Router02.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      },
+      "@uniswap/lib/contracts/libraries/TransferHelper.sol": {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999
+          },
+        }
+      }
     }
   }
 };
